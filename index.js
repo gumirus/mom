2323c750f25ab17b2c063dd17efbdb4fa5f8382f
Ruslan Gumirov <gumirus29@gmail.com>
2025-01-28 21:10:56 +0500

    add js addEventListener

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const output2 = document.querySelector('.output2');
 
 button.addEventListener('click', function () {
   output.innerHTML = 'Мама, я тебя люблю ❤️';
-})
+
+  setTimeout(function () {
+    window.location.href = 'greece.html';
+  }, 3500);
+});
+
 button.addEventListener('click', function () {
   output2.innerHTML = 'Еллада, я тебя люблю ❤️';
-})
 
+  setTimeout(function () {
+    window.location.href = 'index.html';
+  }, 3500);
+});
