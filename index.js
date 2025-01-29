5c4a25a029ecd0bc2dc45ccf25e4c0cb21d76d7a
Ruslan Gumirov <gumirus29@gmail.com>
2025-01-29 10:58:37 +0500

    Improvement of JS code

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const output = document.querySelector('.output');
 const output2 = document.querySelector('.output2');
 
 button.addEventListener('click', function () {
-  output.innerHTML = 'Мама, я тебя люблю ❤️';
+  if (output) {
+    output.innerHTML = 'Мама, я тебя люблю ❤️';
+  }
+
+  if (output2) {
+    output2.innerHTML = 'Еллада, я тебя люблю ❤️';
+  }
 
   setTimeout(function () {
-    window.location.href = 'greece.html';
-  }, 3500);
-});
-
-button.addEventListener('click', function () {
-  output2.innerHTML = 'Еллада, я тебя люблю ❤️';
-
-  setTimeout(function () {
-    window.location.href = 'index.html';
+    if (output) {
+      window.location.href = 'greece.html';
+    } else {
+      window.location.href = 'index.html';
+    }
   }, 3500);
 });
